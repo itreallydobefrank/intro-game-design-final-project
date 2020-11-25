3684207ca1d93dcf82a726db98a066653c1966e4
itreallydobefrank <70737770+itreallydobefrank@users.noreply.github.com>
2020-11-25 02:52:47 -0600

    Added fruit and junk food
    fruit have been added as well as junk food, both with functions to add to or decrease the player health. Player health is also displayed, and sound has been added.

---
+++ Coin-Game-Design-Starter-master/Assets/Scripts/CountFruit.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountFruit : MonoBehaviour
+{
+
+    private int health = 100;
+    public Text countHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countHealth.text = "Health: " + health.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // This is where it will determine what effect each food object will have on player health
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pear"))
+        {
+            health += 5;
+            countHealth.text = "Health: " + health.ToString();
+        }
+
+        if (other.gameObject.CompareTag("Orange"))
+        {
+            health += 10;
+            countHealth.text = "Health: " + health.ToString();
+        }
+
+        if (other.gameObject.CompareTag("Apple"))
+        {
+            health += 15;
+            countHealth.text = "Health: " + health.ToString();
+        }
+
+        if (other.gameObject.CompareTag("Fries"))
+        {
+            health -= 10;
+            countHealth.text = "Health: " + health.ToString();
+        }
+
+        if (other.gameObject.CompareTag("Pizza"))
+        {
+            health -= 15;
+            countHealth.text = "Health: " + health.ToString();
+        }
+    }
+}
---
+++ Coin-Game-Design-Starter-master/Assets/Scripts/FriesBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class FriesBehavior : MonoBehaviour
+{
+    AudioSource myaudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myaudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(PlayAndDestroy(myaudio.clip.length));
+        }
+    }
+
+    private IEnumerator PlayAndDestroy(float waitTime)
+    {
+        myaudio.Play();
+        yield return new WaitForSeconds(waitTime);
+        Destroy(gameObject);
+    }
+
+}
