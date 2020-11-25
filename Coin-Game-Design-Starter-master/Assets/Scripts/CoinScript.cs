eab5c5482f5dd4d4c126779ba42f83d95ef12562
itreallydobefrank <70737770+itreallydobefrank@users.noreply.github.com>
2020-11-25 04:43:19 -0600

    Added Minimap
    Added the minimap feature

--- Coin-Game-Design-Starter-master/Assets/Scripts/CoinScript.cs
+++ Coin-Game-Design-Starter-master/Assets/Scripts/CoinScript.cs
@@ -24,6 +24,10 @@ public class CoinScript : MonoBehaviour
     {
         if (col.gameObject.CompareTag("Player"))
         {
+            Renderer[] allRenderers = gameObject.GetComponentsInChildren<Renderer>();
+            foreach (Renderer c in allRenderers) c.enabled = false;
+            Collider[] allColliders = gameObject.GetComponentsInChildren<Collider>();
+            foreach (Collider c in allColliders) c.enabled = false;
             StartCoroutine(PlayAndDestroy(myaudio.clip.length));
         }
     }
---
+++ Coin-Game-Design-Starter-master/Assets/Scripts/ShowTex.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowTex : MonoBehaviour
+{
+    public Texture aTexture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnGUI()
+    {
+        if (!aTexture)
+        {
+            Debug.LogError("Assign a Texture in the inspector.");
+            return;
+        }
+        //GUI.DrawTexture(new Rect(10, 10, 120, 120), aTexture, ScaleMode.ScaleToFit, true);
+        GUI.DrawTexture(new Rect(Screen.width - 130, 10, 120, 120), aTexture, ScaleMode.ScaleToFit, true);
+    }
+}
