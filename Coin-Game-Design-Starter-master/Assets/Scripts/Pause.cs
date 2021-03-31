50eaa3edaf1cf4ec1d1052d3f7e3f57e182475b4
itreallydobefrank <70737770+itreallydobefrank@users.noreply.github.com>
2021-03-30 21:47:46 -0500

    Added Pause Function
    The game can be paused by pressing the P key. This will also bring up a list of controls

---
+++ Coin-Game-Design-Starter-master/Assets/Scripts/Pause.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject PauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (Time.timeScale == 1)
+            {
+                PauseMenu.SetActive(true);
+                Time.timeScale = 0;
+            }
+            else
+            {
+                PauseMenu.SetActive(false);
+                Time.timeScale = 1;
+            }
+        }
+    }
+
+}
